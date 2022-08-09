f721702f5e3952da2b9e446e663b995c9e5467dd
cclock <nikolay4min99@gmail.com>
2022-08-09 22:21:50 +0400

    v2.0.1

--- cache.go
+++ cache.go
@@ -16,26 +16,25 @@ type CacheInterface interface {
 
 // на каждый уникальный ключ создается горутина, которая ждёт ttl
 // ctx добавлен чтобы завершить горутину, если нужно обновить или удалить ключ
-type cache struct {
-	values map[string]interface{}
-	ttl    map[string]ctxWithCancel
-	mu     sync.RWMutex
-	ctx    context.Context
+type valueItem struct {
+	ttl       time.Duration
+	item      interface{}
+	ctx       context.Context
+	ctxCancel context.CancelFunc
 }
 
-type ctxWithCancel struct {
-	context.Context
-	context.CancelFunc
+type cache struct {
+	values map[string]valueItem
+	mu     sync.RWMutex
 }
 
 func (c *cache) cleanValues(key string, ttl time.Duration) {
 	select {
-	case <-c.ttl[key].Done():
+	case <-c.values[key].ctx.Done():
 		return
 	case <-time.After(ttl):
 		c.mu.Lock()
 		delete(c.values, key)
-		delete(c.ttl, key)
 		c.mu.Unlock()
 	}
 }
@@ -43,14 +42,16 @@ func (c *cache) cleanValues(key string, ttl time.Duration) {
 func (c *cache) Set(key string, value interface{}, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.values[key] = value
-	ctxCancel, exist := c.ttl[key]
-	if exist {
-		ctxCancel.CancelFunc()
-		time.Sleep(time.Second)
+
+	c.cleanIfExist(key)
+	ctx, ctxCancel := context.WithCancel(context.TODO())
+	c.values[key] = valueItem{
+		item:      value,
+		ttl:       ttl,
+		ctx:       ctx,
+		ctxCancel: ctxCancel,
 	}
-	ctx, cancel := context.WithCancel(c.ctx)
-	c.ttl[key] = ctxWithCancel{ctx, cancel}
+
 	go c.cleanValues(key, ttl)
 }
 
@@ -68,30 +69,35 @@ func (c *cache) Delete(key string) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	_, err := c.findEl(key)
-	if err == nil {
-		c.ttl[key].CancelFunc()
+	if err != nil {
+		c.values[key].ctxCancel()
 		delete(c.values, key)
-		// Ожидаем когда контекст завершится
-		time.Sleep(time.Millisecond)
-		delete(c.ttl, key)
+		return err
 	}
+
 	return err
 }
 
 func New() *cache {
 	return &cache{
-		values: make(map[string]interface{}),
-		ttl:    make(map[string]ctxWithCancel),
-		ctx:    context.Background(),
+		values: make(map[string]valueItem),
+	}
+}
+
+func (c *cache) cleanIfExist(key string) {
+	item, exist := c.values[key]
+	if exist {
+		item.ctxCancel()
+		time.Sleep(time.Second)
 	}
 }
 
 // If element exist err == nil
-func (c *cache) findEl(item string) (foundItem interface{}, err error) {
+func (c *cache) findEl(item string) (interface{}, error) {
 	foundItem, exist := c.values[item]
 	if !exist {
-		err = errors.New(fmt.Sprintf("item not found, key = [%s]", item))
+		err := errors.New(fmt.Sprintf("item not found, key = [%s]", item))
 		return nil, err
 	}
-	return foundItem, nil
+	return foundItem.item, nil
 }
